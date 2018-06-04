b282e605b4326520a72368755c493995486ece73
Connell, Brian <brian_connell@baxter.com>
2018-06-04 10:22:41 -0500

    adding initial homework

---
+++ env/config.py
@@ -0,0 +1,7 @@
+consumer_key = "c3PrKI51EZ8DXyJ71EtKok3AL"
+consumer_secret = "IAWEnWZ8VfEwtk4k0wf3U9to555gGYAyPjk14fVjHT5m3djDFt" 
+access_token =  "883758053748871168-2mZJ3BqeqeuPvCNQkq1gDEkgcNOvBYx"
+access_token_secret = "FDNuSKC584j7ivZrBD29tdIMdUMxBfTe5zNp8LDcl2ZVR"
+
+
+weather_api_key="25bc90a1196e6f153eece0bc0b0fc9eb"
